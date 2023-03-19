64dd5d0cdcecd4bb1832f5cd40f879960f015443
A'laa Mohamed Nosser <Nosser@Alaas-MacBook-Air.local>
2023-03-19 06:57:05 +0200

    auth in progress

--- frontend/src/Pages/ExamineePreExam.jsx
+++ frontend/src/Pages/ExamineePreExam.jsx
@@ -9,30 +9,6 @@ const ExamineePreExam = () => {
         className="container w-75 landing-container p-2"
       >
         <form className="m-4 p-3">
-          <div class="row p-2">
-            <div class="form-group col-md-6 p-2">
-              <h5 className="mb-3" for="inputEmail4">
-                الاسم
-              </h5>
-              <input
-                type="email"
-                class="form-control"
-                id="inputEmail4"
-                placeholder="Email"
-              />
-            </div>
-            <div class="form-group col-md-6 p-2">
-              <h5 className="mb-3" for="inputPassword4">
-                رقم الشرطة
-              </h5>
-              <input
-                type="password"
-                class="form-control"
-                id="inputPassword4"
-                placeholder="Password"
-              />
-            </div>
-          </div>
           <div class="form-row p-2">
             <div class="form-group col-md-4">
               <h5 className="mb-3" for="inputState">
@@ -50,10 +26,10 @@ const ExamineePreExam = () => {
                 الاسم
               </h5>
               <input
-                type="email"
+                type="text"
                 class="form-control"
                 id="inputEmail4"
-                placeholder="Email"
+                placeholder="الاسم"
               />
             </div>
             <div class="form-group col-md-6 p-2">
@@ -61,31 +37,65 @@ const ExamineePreExam = () => {
                 رقم الشرطة
               </h5>
               <input
-                type="password"
+                type="text"
                 class="form-control"
                 id="inputPassword4"
-                placeholder="Password"
+                placeholder="رقم الشرطة"
               />
             </div>
           </div>
-          <div class="form-row">
-            <div class="form-group col-md-4">
-              <h5 for="inputState">الرتبة</h5>
+
+          <div class="row p-2">
+            <div class="form-group col-md-6 p-2">
+              <h5 className="mb-3" for="inputEmail4">
+                اسم الفرقة
+              </h5>
+              <input
+                type="text"
+                class="form-control"
+                id="inputEmail4"
+                placeholder="اسم الفرقة"
+              />
+            </div>
+            <div class="form-group col-md-6 p-2">
+              <h5 className="mb-3" for="inputPassword4">
+                رقم الفرقة
+              </h5>
+              <input
+                type="text"
+                class="form-control"
+                id="inputPassword4"
+                placeholder="رقم الفرقة"
+              />
+            </div>
+          </div>
+          <div class="row p-2">
+            <div class="form-group col-md-6 p-2">
+              <h5 className="mb-3" for="inputState">
+                الرتبة
+              </h5>
               <select id="inputState" class="form-control">
                 <option selected>Choose...</option>
                 <option>...</option>
               </select>
             </div>
-          </div>
-          <div class="form-rows">
-            <div class="form-group col-md-2">
-              <h5 for="inputZip">Zip</h5>
-              <input type="text" class="form-control" id="inputZip" />
+            <div class="form-group col-md-6 p-2">
+              <h5 className="mb-3" for="inputPassword4">
+                الجهة المسؤولة
+              </h5>
+              <input
+                type="text"
+                class="form-control"
+                id="inputPassword4"
+                placeholder="الجهة المسؤولة"
+              />
             </div>
           </div>
-          <button type="submit" class="btn btn-primary mt-3">
-            Sign in
-          </button>
+          <div className="row w-50 mt-4">
+            <button type="submit" class="btn btn-primary mt-3 w-25">
+              ابدأ
+            </button>
+          </div>
         </form>
       </div>
     </>
--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/Sidebar.css";
+import logo from "../assets/logo.png";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -12,39 +13,38 @@ import {
 export default function SideBar() {
   return (
     <>
-      <div className="wrapper ">
+      <nav className="wrapper g-0">
         <div className="sidebar bg-light">
-          <h2>Sidebar</h2>
-          <ul>
-            <li>
-              <a href="#">
-                <i className="fas fa-home"></i>
-                <Link to={"/app/home"}>
-                  {" "}
-                  <FontAwesomeIcon icon={faHome} /> &nbsp; Home
-                </Link>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <i className="fas fa-user"></i>
-                <Link to={"/app/questionBanks"}>
-                  <FontAwesomeIcon icon={faBuildingColumns} /> &nbsp;
-                  QuestionBank
-                </Link>
-              </a>
-            </li>
-            <li>
-              <a>
-                <i className="fas fa-user"></i>
-                <Link to={"/app/exams"}>
-                  <FontAwesomeIcon icon={faPen} /> &nbsp; Exam
-                </Link>
-              </a>
-            </li>
-          </ul>
+          <div className="row d-flex justifty-content-center align-items-center">
+            <img src={logo} alt="" />
+          </div>
+          <li>
+            <a className="w-100 h-100" href="#">
+              <i className="fas fa-home"></i>
+              <Link to={"/app/home"}>
+                {" "}
+                <FontAwesomeIcon icon={faHome} /> &nbsp; Home
+              </Link>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <i className="fas fa-user"></i>
+              <Link to={"/app/questionBanks"}>
+                <FontAwesomeIcon icon={faBuildingColumns} /> &nbsp; QuestionBank
+              </Link>
+            </a>
+          </li>
+          <li>
+            <a>
+              <i className="fas fa-user"></i>
+              <Link to={"/app/exams"}>
+                <FontAwesomeIcon icon={faPen} /> &nbsp; Exam
+              </Link>
+            </a>
+          </li>
         </div>
-      </div>
+      </nav>
     </>
   );
 }
